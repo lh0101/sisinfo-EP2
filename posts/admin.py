8e9b91a6ccef8b24075ab80e1a58cbb81c70e2db
Luiz Henrique Piffer Marques <luiz.piffermaques@usp.br>
2024-11-16 15:01:09 -0300

    feat: aplicando comentarios e usuarios e permissoes

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'post_date')
+    ordering = ('-post_date',)
+    search_fields = ('title', 'author__username')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    ordering = ('-created_at',)
+    search_fields = ('post__title', 'author__username')
