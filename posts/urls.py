66b09e9b04fe74e86bea1fc96f61ef0a437ec51e
Luiz Henrique Piffer Marques <luiz.piffermaques@usp.br>
2024-11-16 10:36:27 -0300

    Implementadas views funcionais sem Django Forms

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='posts_list'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('create/', PostCreateView.as_view(), name='posts_create'),
-    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
-    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('', views.posts_list, name='posts_list'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('create/', views.post_create, name='posts_create'),
+    path('<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('<int:post_id>/delete/', views.post_delete, name='post_delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,35 +1,39 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
-# ListView: Exibe a lista de posts
-class PostListView(ListView):
-    model = Post
-    template_name = 'posts/posts_list.html'
-    context_object_name = 'posts'
-    ordering = ['-post_date']
+# Listar todos os posts
+def posts_list(request):
+    posts = Post.objects.all().order_by('-post_date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
 
-# DetailView: Exibe os detalhes de um post
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'posts/post_detail.html'
+# Exibir os detalhes de um post
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'posts/post_detail.html', {'post': post})
 
-# CreateView: Cria um novo post
-class PostCreateView(CreateView):
-    model = Post
-    template_name = 'posts/post_form.html'
-    fields = ['title', 'content']
-    success_url = reverse_lazy('posts_list')
+# Criar um novo post
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+        return redirect('posts_list')
+    return render(request, 'posts/post_form.html')
 
-# UpdateView: Edita um post existente
-class PostUpdateView(UpdateView):
-    model = Post
-    template_name = 'posts/post_form.html'
-    fields = ['title', 'content']
-    success_url = reverse_lazy('posts_list')
+# Editar um post existente
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('post_detail', post_id=post.id)
+    return render(request, 'posts/post_form.html', {'post': post})
 
-# DeleteView: Deleta um post (com página de confirmação)
-class PostDeleteView(DeleteView):
-    model = Post
-    template_name = 'posts/post_confirm_delete.html'
-    success_url = reverse_lazy('posts_list')
+# Deletar um post existente
+def post_delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts_list')
+    return render(request, 'posts/post_confirm_delete.html', {'post': post})
